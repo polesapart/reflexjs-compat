6f7c1fbf02b8c91ffe48015020e79955ab425136
Alexandre Pereira Nunes <alexandre.nunes@gmail.com>
2023-09-06 15:01:01 -0300

    potential fixes

--- src/react-jsx.ts
+++ src/react-jsx.ts
@@ -1,194 +1,197 @@
-/* eslint-disable */
-// noinspection JSUnusedGlobalSymbols,JSUnresolvedReference
+/* eslint-disable @typescript-eslint/strict-boolean-expressions,@typescript-eslint/naming-convention */
+// noinspection JSUnusedGlobalSymbols
 
-import * as React from "react";
-import deepmerge from "deepmerge";
-import { css, jsx as themeUIJSX, ThemeUIContextValue, ThemeUIStyleObject, useThemeUI } from "theme-ui";
-import styleProps from "./style-props.js";
-import { StyleProps, SxProps, Theme, StringIndexedObject } from "./types.js";
+import type * as React from 'react'
+import styleProps from './style-props.js'
+import { type StyleProps, type SxProps, type Theme } from './types.js'
+import deepmerge from 'deepmerge'
+import {
+  jsx as themeUIJSX,
+  css,
+  type ThemeUIStyleObject,
+  useThemeUI,
+  type ThemeUIContextValue
+} from 'theme-ui'
 
+export { useColorMode, css, get, InitializeColorMode } from 'theme-ui'
 
-export { useColorMode, css, get, InitializeColorMode } from "theme-ui";
-
-interface ExactContextValue extends Omit<ThemeUIContextValue, "theme"> {
-    theme: Theme;
+interface ExactContextValue extends Omit<ThemeUIContextValue, 'theme'> {
+  theme: Theme
 }
 
-export const merge = deepmerge;
+export const merge = deepmerge
 
-export const useTheme = (useThemeUI as unknown) as () => ExactContextValue;
+export const useTheme = (useThemeUI as unknown) as () => ExactContextValue
 
-const RESPONSIVE_SEPARATOR = "|";
+const RESPONSIVE_SEPARATOR = '|'
 
-const regex = new RegExp(`^(${Object.keys(styleProps).join("|")})$`);
+const regex = new RegExp(`^(${Object.keys(styleProps).join('|')})$`)
 
-
-
-const omit = (props: {
-    [x: string]: any;
-}) => {
-    const next: StringIndexedObject = {};
-    for (const key in props) {
-        if (regex.test(key))
-            continue;
-        next[key] = props[key];
-    }
-    return next;
-};
-
-const pick = (props: {
-    [x: string]: any;
-}) => {
-    const next : StringIndexedObject = {};
-    for (const key in props) {
-        if (!regex.test(key))
-            continue;
-        next[key] = props[key];
-    }
-    return next;
-};
-
-const split = (props: {
-    [x: string]: any;
-}) => [pick(props), omit(props)];
-
-export const makeResponsive = (prop: string) => {
-
-
-    // Allow responsive values to be written as "foo|bar|baz".
-    return prop.split(RESPONSIVE_SEPARATOR).map((value) => {
-        if (value === "null") {
-            return null;
-        }
-        return value.match(/^\d+$/) ? parseInt(value) : value;
-    });
-};
-
-export function transformProps(props: StyleProps, result: StringIndexedObject = {}): ThemeUIStyleObject {
-    if (props !== null && typeof props === "object") {
-        Object.entries(props).forEach(([key, value]) => {
-            if (!Array.isArray(value) && typeof value === "object") {
-                return (result[key.replace(/^_/, ":")] = transformProps(value));
-            }
-
-            if (typeof styleProps[key] !== "undefined") {
-                const names = styleProps[key];
-                names.forEach((name: string | number) => {
-                    result[name] = makeResponsive(value);
-                });
-            }
-        });
-    }
-
-    return result;
+const omit = (props: Record<string, any>): Record<string, any> => {
+  const next: Record<string, any> = {}
+  for (const key in props) {
+    if (regex.test(key)) continue
+    next[key] = props[key]
+  }
+  return next
 }
 
-type doNotParseType = keyof React.JSX.IntrinsicElements;
+const pick = (props: Record<string, any>): Record<string, any> => {
+  const next: Record<string, any> = {}
+  for (const key in props) {
+    if (!regex.test(key)) continue
+    next[key] = props[key]
+  }
+  return next
+}
 
-const doNotParseTypes: doNotParseType[] = ["meta"];
+const split = (props: Record<string, any>): [Record<string, any>, Record<string, any>] => [pick(props), omit(props)]
+
+export const makeResponsive = (prop: string | any): any => {
+  if (typeof prop !== 'string') {
+    return prop
+  }
+
+  // Allow responsive values to be written as "foo|bar|baz".
+  return prop.split(RESPONSIVE_SEPARATOR).map((value) => {
+    if (value === 'null') {
+      return null
+    }
+    return (value.match(/^\d+$/) != null) ? parseInt(value) : value
+  })
+}
+
+export function transformProps (
+  props: StyleProps,
+  result: Record<string, any> = {}
+): ThemeUIStyleObject {
+  if (props !== null && typeof props === 'object') {
+    Object.entries(props).forEach(([key, value]) => {
+      if (!Array.isArray(value) && typeof value === 'object') {
+        return (result[key.replace(/^_/, ':')] = transformProps(value))
+      }
+
+      if (typeof styleProps[key] !== 'undefined') {
+        const names = styleProps[key]
+        names.forEach((name: string | number) => {
+          result[name] = makeResponsive(value)
+        })
+      }
+    })
+  }
+
+  return result
+}
+
+type doNotParseType = keyof React.JSX.IntrinsicElements
+
+const doNotParseTypes: doNotParseType[] = ['meta']
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-function isDoNotParseType(name: any): name is doNotParseType {
-    return doNotParseTypes.includes(name);
+function isDoNotParseType (name: any): name is doNotParseType {
+  return doNotParseTypes.includes(name)
 }
 
-export function parseProps<P>(type: React.ElementType<P> | Object, props: any) {
-    if (!props)
-        return props;
+export function parseProps (type: any, props: any): any {
+  if (!props) return props
 
-    if (isDoNotParseType(type)) {
-        return props;
-    }
+  if (isDoNotParseType(type)) {
+    return props
+  }
 
-    // Fix for React.Fragment.
-    if (typeof type === "symbol") {
-        return props;
-    }
+  // Fix for React.Fragment.
+  if (typeof type === 'symbol') {
+    return props
+  }
 
-    if (typeof type !== "string") {
-        return props["sx"]
-            ? {
-                ...props,
-                sx: transformProps(props.sx),
-            }
-            : props;
-    }
-
-    const { sx: _sx = {}, ...__props } = props;
-    let { variant, ..._props }: Omit<any, "sx"> = __props;
-    const { variant: sxVariant, ...sx } = _sx;
-    if (sxVariant) {
-        variant = sxVariant;
-    }
-
-    const [styleProps, otherProps] = split(_props);
-
-    if (!variant &&
-        Object.keys(sx).length === 0 &&
-        Object.keys(styleProps).length === 0)
-        return props;
-
-    const sxProps = transformProps(deepmerge(styleProps, sx));
-
-    const next: typeof props & {
-        /* eslint-disable  @typescript-eslint/no-explicit-any */
-        css?: any;
-        sx: SxProps;
-    } = {
-        ...otherProps,
-    };
-
-    if (!variant) {
-        next.sx = sxProps;
-        return next;
-    }
-
-    next.css = (theme: ({
-        theme: Theme
-        [key: string]: any
-    } | Theme)) => {
-        const variants = variant.split(" ");
-        let __themeKeyFailed = false;
-        let variantStyles = {};
-        variants.forEach((variant: string) => {
-            const [__themeKey, ...nestedVariants] = variant.split(".");
-            if (!theme[__themeKey]) {
-                __themeKeyFailed = true;
-                return;
-            }
-
-            // Handle nested variants.
-            let styles = theme[__themeKey];
-            nestedVariants.forEach((v) => {
-                if (theme[__themeKey][v]) {
-                    const _vStyles = theme[__themeKey][v];
-                    const vStyles = typeof _vStyles === "function" ? _vStyles(theme, styles) : _vStyles;
-                    styles = deepmerge(styles, vStyles);
-                }
-            });
-
-            variantStyles = deepmerge(variantStyles, styles);
-        });
-
-        if (__themeKeyFailed) {
-            return sxProps ? css(sxProps)(theme) : null;
+  if (typeof type !== 'string') {
+    return props.sx
+      ? {
+          ...props,
+          sx: transformProps(props.sx)
         }
+      : props
+  }
 
-        return css(transformProps({
-            ...variantStyles,
-            ...sxProps as any,
-        }))(theme);
-    };
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { sx: _sx = {}, ...__props } = props
+  let { variant, ..._props } = __props
+  const { variant: sxVariant, ...sx } = _sx
+  if (sxVariant) {
+    variant = sxVariant
+  }
 
-    return next;
+  const [styleProps, otherProps] = split(_props)
+
+  if (
+    !variant &&
+        Object.keys(sx).length === 0 &&
+        Object.keys(styleProps).length === 0
+  ) {
+    return props
+  }
+
+  const sxProps = transformProps(deepmerge(styleProps, sx))
+
+  const next: typeof props & {
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
+    css?: any
+    sx: SxProps
+  } = {
+    ...otherProps
+  }
+
+  if (!variant) {
+    next.sx = sxProps
+    return next
+  }
+
+  next.css = (theme: Record<string, any>) => {
+    const variants = variant.split(' ')
+    let __themeKeyFailed = false
+    let variantStyles = {}
+    variants.forEach((variant: { split: (arg0: string) => [any, ...any[]] }) => {
+      const [__themeKey, ...nestedVariants] = variant.split('.')
+      if (!theme[__themeKey]) {
+        __themeKeyFailed = true
+        return
+      }
+
+      // Handle nested variants.
+      let styles = theme[__themeKey]
+      nestedVariants.forEach((v) => {
+        if (theme[__themeKey][v]) {
+          const _vStyles = theme[__themeKey][v]
+          const vStyles =
+                        typeof _vStyles === 'function' ? _vStyles(theme, styles) : _vStyles
+          styles = deepmerge(styles, vStyles)
+        }
+      })
+
+      variantStyles = deepmerge(variantStyles, styles)
+    })
+
+    if (__themeKeyFailed) {
+      return sxProps ? css(sxProps)(theme) : null
+    }
+
+    return css(
+      transformProps({
+        ...variantStyles,
+        ...sxProps as any
+      })
+    )(theme)
+  }
+
+  return next
 }
 
-// @ts-ignore
+// @ts-expect-error not yet
 export const jsx: typeof React.createElement = (type, props, ...children) => {
-    return themeUIJSX.apply(undefined, [
-        type,
-        parseProps(type, props),
-        ...children,
-    ]);
-};
-
+  // eslint-disable-next-line no-useless-call
+  return themeUIJSX.apply(undefined, [
+    type,
+    parseProps(type, props),
+    ...children
+  ])
+}
